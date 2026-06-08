8f288871a221ed796f93621af628c3b7f38e4c48
Lucien-DM <lucien@nuggetcomputersinc.net>
2026-06-08 20:30:47 +1000

    new file:   hangman.py - added program file, built some functions new file:   logger.py - added log library github.com/Lucien-DM/python-logger

---
+++ hangman.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import random
+import logger as log
+FILE_WORDS = "words.txt"
+ENCODING = 'utf-8'
+
+def does_file_exist(file):
+    """Checks if file exists, and if you have permission to read it"""
+    try:
+        with open(file, 'r'):
+            pass
+    except FileNotFoundError:
+        return False
+    except PermissionError:
+        return False
+    else:
+        return True
+
+def import_words(path):
+    """Imports Works from file"""
+    with open(path, 'r', encoding=ENCODING) as f:
+        x = 0
+        words = []
+        for line in f:
+            word = [x, line.read()]
+            words.append(word)
+            x += 1
+        return words
+
+def letter_in_word(word, letter):
+    if letter in word:
+        
